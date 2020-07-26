701fb898a5ef97c67195ef75cb84a54073934f22
yaminipapillugari <yaminireddy12345p@gmail.com>
2020-07-26 12:41:13 +0530

    first commit

---
+++ Java8-master/streams/src/main/java/Lambdas/streams/Palin.java
@@ -0,0 +1,32 @@
+package Lambdas.streams;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Palin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		List<StringBuilder>a=new ArrayList <StringBuilder>();
+		StringBuilder s1=new StringBuilder("mom");
+		StringBuilder s2=new StringBuilder("hello");
+		StringBuilder s3=new StringBuilder("maadaam");
+		StringBuilder s4=new StringBuilder("kikckik");
+		a.add(s1);
+		a.add(s2);
+		a.add(s3);
+		a.add(s4);
+		for (StringBuilder x:a) {
+			System.out.println(x);
+		}
+		List<StringBuilder> st=a.stream().filter(p->p==p.reverse()).collect(Collectors.toList());
+		System.out.println("Palindromes are...");
+		for (StringBuilder x:st) {
+			System.out.println(x.reverse());
+		}
+	s.close();
+	}
+
+}
---
+++ Java8-master/streams/src/main/java/Lambdas/streams/Slen.java
@@ -0,0 +1,27 @@
+package Lambdas.streams;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Slen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		List<String>a=new ArrayList<String>();
+		int n=s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			a.add(s.next());
+		}
+		List<String> st=a.stream().filter(p->p.startsWith("a")).filter(t->t.length()==3).collect(Collectors.toList());
+		System.out.println("the strings are");
+		for(String x:st)
+		{
+			System.out.println(x);
+		}
+		s.close();
+	}
+
+}
